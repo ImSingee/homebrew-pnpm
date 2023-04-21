8232b94fa5d87b296d0c2b7bb0bfb2722a258ec6
Singee <git@singee.me>
2023-04-21 15:18:01 +0800

    remove bottles

--- pnpm@7.rb
+++ pnpm@7.rb
@@ -12,16 +12,6 @@ class PnpmAT7 < Formula
       regex(/["']latest-7["']:\s*?["']([^"']+)["']/i)
     end
   
-    bottle do
-      sha256 cellar: :any_skip_relocation, arm64_ventura:  "78ecd13f60c3baf6913933c8494ca17fc4e5b9f93c46bbc131312ffe41fe7f88"
-      sha256 cellar: :any_skip_relocation, arm64_monterey: "78ecd13f60c3baf6913933c8494ca17fc4e5b9f93c46bbc131312ffe41fe7f88"
-      sha256 cellar: :any_skip_relocation, arm64_big_sur:  "78ecd13f60c3baf6913933c8494ca17fc4e5b9f93c46bbc131312ffe41fe7f88"
-      sha256 cellar: :any_skip_relocation, ventura:        "2f4f18876a3e2823f86f5500b7c47c173695e7f21eba007c2b7689dd12301145"
-      sha256 cellar: :any_skip_relocation, monterey:       "2f4f18876a3e2823f86f5500b7c47c173695e7f21eba007c2b7689dd12301145"
-      sha256 cellar: :any_skip_relocation, big_sur:        "4be656f6ff04e145810fb6e19f08fb01030798cec610c9d618b1fb01121d9f64"
-      sha256 cellar: :any_skip_relocation, x86_64_linux:   "78ecd13f60c3baf6913933c8494ca17fc4e5b9f93c46bbc131312ffe41fe7f88"
-    end
-  
     depends_on "node" => :test
   
     conflicts_with "corepack", because: "both installs `pnpm` and `pnpx` binaries"
